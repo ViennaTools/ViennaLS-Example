3460f311b406565c76cb231803eb2bf2a25f03c9
Xaver K <klemenschits@iue.tuwien.ac.at>
2019-12-20 15:04:33 +0100

    Updated to new version.

--- Frosty.cpp
+++ Frosty.cpp
@@ -4,13 +4,15 @@
 */
 
 #include <iostream>
+#include <array>
 
 #include <lsBooleanOperation.hpp>
 #include <lsDomain.hpp>
 #include <lsExpand.hpp>
+#include <lsGeometries.hpp>
 #include <lsMakeGeometry.hpp>
 #include <lsPrune.hpp>
-#include <lsToExplicitMesh.hpp>
+#include <lsToSurfaceMesh.hpp>
 #include <lsVTKWriter.hpp>
 #include <lsAdvect.hpp>
 
@@ -18,8 +20,8 @@
 class velocityField : public lsVelocityField<double> {
 public:
   double getScalarVelocity(
-      hrleVectorType<double, 3> /*coordinate*/, int material,
-      hrleVectorType<double, 3> /*normalVector = hrleVectorType<double, 3>(0.)*/) {
+      const std::array<double, 3> & /*coordinate*/, int material,
+      const std::array<double, 3> &/*normalVector = hrleVectorType<double, 3>(0.)*/) {
     // Some arbitrary velocity function of your liking
     // (try changing it and see what happens :)
     double velocity;
@@ -31,13 +33,6 @@ public:
       case 2: velocity = -1;
     }
     return velocity;
-  }
-
-  hrleVectorType<double, 3> getVectorVelocity(
-      hrleVectorType<double, 3> /*coordinate*/, int /*material*/,
-      hrleVectorType<double,
-                     3> /*normalVector = hrleVectorType<double, 3>(0.)*/) {
-    return hrleVectorType<double, 3>(0.);
   }
 };
 
@@ -55,30 +50,30 @@ int main() {
   double origin[3] = {0., 0., 0.};
   double radius = 10.0;
 
-  lsMakeGeometry<double, D>(sphere1).makeSphere(origin, radius);
+  lsMakeGeometry<double, D>(sphere1, lsSphere<double, D>(origin, radius)).apply();
   origin[2] = 12.0;
   radius = 7.2;
-  lsMakeGeometry<double, D>(sphere2).makeSphere(origin, radius);
+  lsMakeGeometry<double, D>(sphere2, lsSphere<double, D>(origin, radius)).apply();
   origin[2] = 20.0;
   radius = 5.0;
-  lsMakeGeometry<double, D>(sphere3).makeSphere(origin, radius);
+  lsMakeGeometry<double, D>(sphere3, lsSphere<double, D>(origin, radius)).apply();
 
   // Perform a boolean operation
-  lsBooleanOperation<double, D>(sphere2).OR(sphere3);
-  lsBooleanOperation<double, D>(sphere1).OR(sphere2);
+  lsBooleanOperation<double, D>(sphere2, sphere3, lsBooleanOperationEnum::UNION).apply();
+  lsBooleanOperation<double, D>(sphere1, sphere2, lsBooleanOperationEnum::UNION).apply();
 
   std::cout << "Extracting..." << std::endl;
   {
     lsMesh mesh1, mesh2, mesh3;
 
     std::cout << "Extracting..." << std::endl;
-    lsToExplicitMesh<double, D>(sphere1, mesh1).apply();
-    lsToExplicitMesh<double, D>(sphere2, mesh2).apply();
-    lsToExplicitMesh<double, D>(sphere3, mesh3).apply();
+    lsToSurfaceMesh<double, D>(sphere1, mesh1).apply();
+    lsToSurfaceMesh<double, D>(sphere2, mesh2).apply();
+    lsToSurfaceMesh<double, D>(sphere3, mesh3).apply();
 
-    lsVTKWriter(mesh1).writeVTKLegacy("sphere1-0.vtk");
-    lsVTKWriter(mesh2).writeVTKLegacy("sphere2-0.vtk");
-    lsVTKWriter(mesh3).writeVTKLegacy("sphere3-0.vtk");
+    lsVTKWriter(mesh1, "sphere1-0.vtk").apply();
+    lsVTKWriter(mesh2, "sphere2-0.vtk").apply();
+    lsVTKWriter(mesh3, "sphere3-0.vtk").apply();
   }
 
 
@@ -94,7 +89,9 @@ int main() {
   lsAdvect<double, D> advection(lsDomains, velocities);
   // advection.setIntegrationScheme(1);
   advection.setCalculateNormalVectors(false);
-  double advectionSteps = advection.apply(1.0);
+  advection.setAdvectionTime(1.0);
+  advection.apply();
+  double advectionSteps = advection.getNumberOfTimeSteps();
   std::cout << "Number of Advection steps taken: " << advectionSteps
             << std::endl;
 
@@ -103,13 +100,13 @@ int main() {
     lsMesh mesh1, mesh2, mesh3;
 
     std::cout << "Extracting..." << std::endl;
-    lsToExplicitMesh<double, D>(sphere1, mesh1).apply();
-    lsToExplicitMesh<double, D>(sphere2, mesh2).apply();
-    lsToExplicitMesh<double, D>(sphere3, mesh3).apply();
+    lsToSurfaceMesh<double, D>(sphere1, mesh1).apply();
+    lsToSurfaceMesh<double, D>(sphere2, mesh2).apply();
+    lsToSurfaceMesh<double, D>(sphere3, mesh3).apply();
 
-    lsVTKWriter(mesh1).writeVTKLegacy("sphere1-1.vtk");
-    lsVTKWriter(mesh2).writeVTKLegacy("sphere2-1.vtk");
-    lsVTKWriter(mesh3).writeVTKLegacy("sphere3-1.vtk");
+    lsVTKWriter(mesh1, "sphere1-1.vtk").apply();
+    lsVTKWriter(mesh2, "sphere2-1.vtk").apply();
+    lsVTKWriter(mesh3, "sphere3-1.vtk").apply();
   }
 
   return 0;
